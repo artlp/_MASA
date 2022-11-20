35c8cd1a8ec8f1a27d86b3574bc00ed5b11a0d65
Artur Lipskii <falcon.arthur@gmail.com>
2022-11-20 09:17:04 +0200

    soccer manager

--- soccer-manager/app.js
+++ soccer-manager/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 const path = require('path');
+const fileUpload = require("express-fileupload");
 require('ejs');
-const {getHomePage} = require('./routes/home'); 
-const {getAddPage} = require('./routes/player'); 
+const { getHomePage } = require('./routes/home');
+const { getAddPage, addPlayer } = require('./routes/player');
 
 let port = 8000;
 port = process.env.port || port;
@@ -16,25 +17,36 @@ const db = mysql.createConnection({
     user: 'fan',
     password: '123',
     database: 'soccer_manager'
-})
+});
 db.connect(err => {
     if (err) {
         //!connection to db is critical for this app, so we should stop the app if connection is not established.
         throw err;
     }
     console.log("Connection to the database is established.");
-})
+});
 
 global.db = db;
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(fileUpload());
 
 app.get('/', getHomePage);
+app.post('/add', addPlayer);
 app.get('/add', getAddPage);
+
+//* HTTP REQUEST METHODS: 
+//* GET = to get a page (probably with data) - address line with parameters
+//* POST = insert data into a server, usually via forms.
+//* PUT = insert if there's no data, update if data already exists
+//* DELETE - delete data
+//* etc
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 //* MVC = Model View Controller
- 
--- soccer-manager/routes/player.js
+++ soccer-manager/routes/player.js
@@ -7,7 +7,37 @@ module.exports = {
             });
     },
     addPlayer: (req, res) => {
-        
+        if (!req.files) {
+            res.status(400).send("No files were uploaded");
+        }
+        //*req.body was parsed by express and now it's an object with properties
+        //* as the names of the input fields
+        let message = '';
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        let userName = req.body.username;
+        let number = req.body.first_name;
+        let position = req.body.number;
+        let image = req.files.image;
+        let extension = image.mimetype.split('/')[1];
+        let imageName = userName + '.' + extension;
+        console.log(imageName);
+        let query = `SELECT * FROM players WHERE user_name = '${userName}'`;
+        db.query(query, (err, result) => {
+            if (err) {
+                res.status(500).send(`Query for existing user, err: \n`, err);
+            }
+            if (result.length > 0) {
+                message = 'Error: this player already exists';
+                res.render('add-player.ejs',
+                {
+                    title: 'Soccer-Manager | Add player',
+                    message: message,
+                });
+            }
+        });
+
+        console.log("addplayer");
     },
     editPlayer: (req, res) => {
 
