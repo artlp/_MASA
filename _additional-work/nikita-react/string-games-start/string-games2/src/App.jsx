23e93e3514fb48ef9e893c303c65cee186397690
Artur Lipskii <falcon.arthur@gmail.com>
2022-11-27 11:45:01 +0200

    react gematria additional

---
+++ _additional-work/nikita-react/string-games-start/string-games2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Gematria } from './components/gematria.jsx';
+
+function App() {
+    return (
+        <div className="App">
+            <section className="above">            <Gematria></Gematria></section>
+            <section className="below"></section>
+        </div>
+    );
+}
+
+export default App;
---
+++ _additional-work/nikita-react/string-games-start/string-games2/src/components/gematria.jsx
@@ -0,0 +1,28 @@
+import { useRef, useState } from "react";
+import { calculateGematriaValue } from "../utils/gematria";
+
+export function Gematria() {
+
+    // const [inputValue, setInputValue] = useState(' ');
+    // const handleInputChange = (event) => {
+    //     setInputValue(event.target.value);
+    // };
+    const [points, setPoints] = useState('0');
+    const input = useRef();
+    const handleButtonClick = () => {
+        const value = input.current.value;
+        const calculatedPoints = calculateGematriaValue(value);
+        setPoints(calculatedPoints);
+    };
+
+    return (
+        <div className="gematria">
+            <h2>גימטריה</h2>
+            <div className="gem-container">
+                <input ref={input} type='text' className="gem-input" placeholder="שלןם" />
+                <button onClick={handleButtonClick} className="gem-calc">Calculate</button>
+                <div className="res-calc">{points}</div>
+            </div>
+            <p className="desc">"Gematryia" is the word numerical value calculation while summarizing the values of the letters of Hebrew alphabet. Enter your name in Hebrew and press the button - get value.</p>
+        </div>);
+}
